22b58aab2680585982388b91f8e85c359cfa6aa0
Sam <samknight4321@gmail.com>
2021-02-11 18:19:45 +0000

    Updated gitignore and premake

--- premake5.lua
+++ premake5.lua
@@ -7,15 +7,16 @@ project "imgui"
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
+		"imconfig.h",
+		"imgui.h",
+		"imgui_internal.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_widgets.cpp",
+		"imgui_tables.cpp",
 		"imgui_demo.cpp"
 	}
 
